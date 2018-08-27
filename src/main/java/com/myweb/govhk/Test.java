d6a390d5601ed6d44a410f3eccbb7a2935c567ef
wangyongst <wangyongst@gmail.com>
2018-08-27 18:01:24 +0800

    office

--- src/main/java/com/myweb/govhk/Test.java
+++ src/main/java/com/myweb/govhk/Test.java
@@ -18,9 +18,21 @@ public class Test {
         webClient.getOptions().setRedirectEnabled(true);
         HtmlPage page = webClient.getPage("https://www.gov.hk/sc/residents/taxes/etax/services/brn_enquiry.htm");
         System.out.println(page.asXml());
-        page = page.getAnchorByHref("/sc/apps/irdbrnenquiry.htm").click();
+        page = page.getAnchorByText("立即查询").click();
         Thread.sleep(10000);
         page = page.getElementsByTagName("input").get(0).click();
+        page = page.getAnchorByText("開始使用服務").click();
+        page = page.getAnchorByText("我已閱讀上述收集個人資料聲明及說明").click();
+        page.getElementsByTagName("input").forEach(e->{
+            if(e.getAttribute("value").equals("BRE")) {
+                try {
+                    e.click();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        });
+        page = page.getAnchorByText("繼續").click();
         System.out.println(page.asXml());
 
     }
