b9f5ac5929cc746ba10898666cbb0f294b8e8b2c
wangyongst <wangyongst@gmail.com>
2018-08-28 11:22:37 +0800

    office

--- src/main/java/com/myweb/govhk/Test.java
+++ src/main/java/com/myweb/govhk/Test.java
@@ -2,29 +2,59 @@ package com.myweb.govhk;
 
 import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlImage;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.HtmlSelect;
+import net.sourceforge.tess4j.ITesseract;
+import net.sourceforge.tess4j.Tesseract;
 
+import javax.imageio.ImageReader;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 
 public class Test {
 
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws Exception {
+        Test test = new Test();
+        HtmlPage page = test.initPage();
+        page.getElementByName("companyName").setAttribute("value", "CA SERVICES LIMITED");
+        ((HtmlSelect) page.getElementByName("businessAddArea")).getOptionByValue("A").click();
+        final HtmlImage[] valiCodeImg = {null};
+        page.getElementsByTagName("img").forEach(e -> {
+            if (e.getAttribute("src").contains("captchaServlet")) {
+                valiCodeImg[0] = (HtmlImage) e;
+            }
+        });
+        ImageReader imageReader = valiCodeImg[0].getImageReader();
+        BufferedImage bufferedImage = imageReader.read(0);
+        page.getElementByName("captchaStr").setAttribute("value", getCode(bufferedImage));
+        System.out.println(page.asXml());
+    }
+
+
+    public static String getCode(BufferedImage bufferedImage) throws Exception {
+        ITesseract instance = new Tesseract();
+        BufferedImage textImage = ClearImageHelper.cleanImage(bufferedImage);
+        return instance.doOCR(textImage).trim();
+    }
+
+
+    public HtmlPage initPage() throws IOException, InterruptedException {
         WebClient webClient = new WebClient(BrowserVersion.CHROME);
         webClient.getOptions().setUseInsecureSSL(true);
-        webClient.getOptions().setCssEnabled(false);
+        webClient.getOptions().setCssEnabled(true);
         webClient.getOptions().setAppletEnabled(true);
         webClient.getOptions().setActiveXNative(true);
         webClient.getOptions().setJavaScriptEnabled(true);
         webClient.getOptions().setRedirectEnabled(true);
         HtmlPage page = webClient.getPage("https://www.gov.hk/sc/residents/taxes/etax/services/brn_enquiry.htm");
-        System.out.println(page.asXml());
-        page = page.getAnchorByText("立即查询").click();
+        page = page.getAnchorByHref("/sc/apps/irdbrnenquiry.htm").click();
         Thread.sleep(10000);
         page = page.getElementsByTagName("input").get(0).click();
         page = page.getAnchorByText("開始使用服務").click();
         page = page.getAnchorByText("我已閱讀上述收集個人資料聲明及說明").click();
-        page.getElementsByTagName("input").forEach(e->{
-            if(e.getAttribute("value").equals("BRE")) {
+        page.getElementsByTagName("input").forEach(e -> {
+            if (e.getAttribute("value").equals("BRE")) {
                 try {
                     e.click();
                 } catch (IOException e1) {
@@ -32,8 +62,6 @@ public class Test {
                 }
             }
         });
-        page = page.getAnchorByText("繼續").click();
-        System.out.println(page.asXml());
-
+        return page.getAnchorByText("繼續").click();
     }
 }
