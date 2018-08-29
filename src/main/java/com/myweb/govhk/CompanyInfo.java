1f1792fefaf082578e26171200b9327552cfa3b3
wangyongst <wangyongst@gmail.com>
2018-08-29 17:45:03 +0800

    office

--- src/main/java/com/myweb/govhk/CompanyInfo.java
+++ src/main/java/com/myweb/govhk/CompanyInfo.java
@@ -9,4 +9,68 @@ public class CompanyInfo {
     private String cortename;
     private String branchname;
     private String branchename;
+
+    public String getNo() {
+        return no;
+    }
+
+    public void setNo(String no) {
+        this.no = no;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getInfono() {
+        return infono;
+    }
+
+    public void setInfono(String infono) {
+        this.infono = infono;
+    }
+
+    public String getCorpname() {
+        return corpname;
+    }
+
+    public void setCorpname(String corpname) {
+        this.corpname = corpname;
+    }
+
+    public String getCortename() {
+        return cortename;
+    }
+
+    public void setCortename(String cortename) {
+        this.cortename = cortename;
+    }
+
+    public String getBranchname() {
+        return branchname;
+    }
+
+    public void setBranchname(String branchname) {
+        this.branchname = branchname;
+    }
+
+    public String getBranchename() {
+        return branchename;
+    }
+
+    public void setBranchename(String branchename) {
+        this.branchename = branchename;
+    }
 }
